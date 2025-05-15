833cf2d0340e941db99d38cc38af77c20fcb5125
Student-2024 <student-2024@ashalim.org>
2025-05-15 15:26:07 +0300

    150525

--- app/src/main/java/com/example/studybuddy/adapters/LessonAdapter.java
+++ app/src/main/java/com/example/studybuddy/adapters/LessonAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.studybuddy.R;
@@ -19,6 +20,7 @@ import java.util.List;
 public class LessonAdapter extends ArrayAdapter<Lesson> {
     Context context;
     List<Lesson> objects;
+
 
     public LessonAdapter(Context context, List<Lesson> objects) {
         super(context, 0, objects);
@@ -38,6 +40,7 @@ public class LessonAdapter extends ArrayAdapter<Lesson> {
         TextView tvHour = convertView.findViewById(R.id.tvHourAdapter);
         TextView tvStudent = convertView.findViewById(R.id.tvStudentNameAdapter);
 
+        Button button1 = null,button2 = null;
         Lesson temp = objects.get(position);
         Log.d("LessonAdapter", "Lesson: " + temp.toString());
 
@@ -49,12 +52,42 @@ public class LessonAdapter extends ArrayAdapter<Lesson> {
         if (context instanceof teacherSchedule) {
             Log.d("ContextCheck", "This context is an Activity");
 
-              tvStudent.setText(temp.getStudent().getFname()+"  "+temp.getStudent().getLname());
+            tvStudent.setText(temp.getStudent().getFname()+"  "+temp.getStudent().getLname());
+
+            button1.findViewById(R.id.btAccept);
+            button1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    temp.setStatus(true);
+                    updateLesson(temp);
+                }
+            });
+            button2.findViewById(R.id.btDecline);
+            button2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    temp.setStatus(false);
+                    updateLesson(temp);
+                }
+            });
         }
 
 
         else  tvStudent.setText(temp.getTeacher().getFname()+"  "+temp.getTeacher().getLname());
 
         return convertView;
+    }
+
+    private void updateLesson(Lesson lesson) {
+        DatabaseService.getInstance().createNewLesson(lesson, new DatabaseService.DatabaseCallback<Void>() {
+            @Override
+            public void onCompleted(Void object) {
+
+            }
+            @Override
+            public void onFailed(Exception e) {
+
+            }
+        });
     }
 }
